63c70db75208cc6d240e16ad102ecb540ca50aca
Your Name <you@example.com>
2025-04-09 21:25:47 -0400

    Cambios en extra_tickets

--- TDAs/extra_tickets.h
+++ TDAs/extra_tickets.h
@@ -5,17 +5,24 @@
 
 // Pertenecientes al main
 
-void mostrarMenuPrincipal() ; // Muestra el menú de las opciones del programa.
-void esperarEnter() ; // Espera a que el usuario ingrese ENTER para continuar.
+// Muestra el menú de las opciones del programa.
+void mostrarMenuPrincipal() ; 
+// Espera a que el usuario ingrese ENTER para continuar.
+void esperarEnter() ; 
 
 // Pertenecientes al tickets
 
-int IDenLista(int, List*) ; // Recorre una lista hasta encontrar cierto ID
-int listaEstaVacia(List*) ; // Verifica si es que la lista se encuentra vacía o no. Devuelve 1 si está vacía, y 0 si no.
-void asignarTiempo(char*) ; // Asigna un string con el tiempo actual del sistema.
-void imprimirSeparador(char*, char*) ; // Imprime separadores. La primer variable siendo el separador, y la segunda el mensaje.
-
-void imprimirTicket(tipoTicket*) ; // Imprime los datos del ticket correspondiente.
-int ordenar(void*, void*) ; // Ordena dos variables de cualquier dato. Para Tickets, ordena por prioridad y hora.
+// Recorre una lista hasta encontrar cierto ID
+int IDenLista(int, List*) ; 
+// Verifica si es que la lista se encuentra vacía o no. Devuelve 1 si está vacía, y 0 si no.
+int listaEstaVacia(List*) ; 
+// Asigna un string con el tiempo actual del sistema.
+void asignarTiempo(char*) ; 
+// Imprime separadores. La primer variable siendo el separador, y la segunda el mensaje.
+void imprimirSeparador(char*, char*) ; 
+// Imprime los datos del ticket correspondiente.
+void imprimirTicket(tipoTicket*) ; 
+// Ordena dos variables de cualquier dato. Para Tickets, ordena por prioridad y hora.
+int ordenar(void*, void*) ; 
 
 #endif
